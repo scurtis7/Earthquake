00d7c64d551641d313543a795864dd7ca9762193
Steve Curtis <steve.curtis@truthmd.com>
2026-07-29 14:12:24 -0400

    add version at startup

--- earthquakes/src/main/java/com/scurtis/earthquakes/config/AppConfig.java
+++ earthquakes/src/main/java/com/scurtis/earthquakes/config/AppConfig.java
@@ -1,5 +1,10 @@
 package com.scurtis.earthquakes.config;
 
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
@@ -9,8 +14,19 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import static com.scurtis.earthquakes.common.AppConstants.USGS_BASE_URL;
 
+@Slf4j
+@Getter
+@Setter
 @Configuration
+@ConfigurationProperties(prefix = "scurtis")
 public class AppConfig {
+
+    private String appVersion;
+
+    @PostConstruct
+    public void init() {
+        log.info("Earthquakes Server Version: {}", appVersion);
+    }
 
     @Bean
     public WebClient webClient() {
